eec77e1a3baa885fb441b3a8aa89a51899cf2b7e
jakebark <jakebark@amazon.com>
2026-01-11 21:16:56 +0000

    revert policies

--- modules/policy_attach/main.tf
+++ modules/policy_attach/main.tf
@@ -3,6 +3,6 @@
 
 resource "aws_organizations_policy_attachment" "this" {
   for_each  = toset(var.policies)
-  policy_id = var.policy_id[each.value].id
+  policy_id = var.policy_id["${var.policies_directory_name}/${each.value}.json"].id
   target_id = var.ou
 }
